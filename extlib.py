84eee04edd6f5dfbfaada697752308c78754014d
zwang20 <michael_wzq@icloud.com>
2021-11-08 18:51:57 +1100

    Add experiment mode
    experiment
    
    clean up
    
    minor changes
    
    Merge pull request #1 from zwang20/experimental
    
    Experimental

---
+++ extlib.py
@@ -0,0 +1,27 @@
+"""
+extlib made by Michael.
+
+includes sgn()
+"""
+
+def sgn0(num: float) -> float:
+    if num < 0:
+        return -1
+    return 1
+
+def sgn(num: float) -> float:
+    """
+    Return the sign of x.
+
+    >>> sgn(0)
+    0
+    >>> sgn(1)
+    1
+    >>> sgn(-1)
+    -1
+    """
+    if num > 0:
+        return 1
+    if num < 0:
+        return -1
+    return 0
